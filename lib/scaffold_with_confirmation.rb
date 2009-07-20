ec06490d64937d4afdd3f92b62e58753da2e95e3
Taku Amano <taku@toi-planning.net>
2009-07-20 12:52:46 +0900

    Used a option of resources.

--- lib/scaffold_with_confirmation.rb
+++ lib/scaffold_with_confirmation.rb
@@ -1,28 +1,25 @@
 module ActionController
   module Resources
     def resources_with_confirmation(*entities, &block)
-      #resources
       options = entities.extract_options!
-      entities.each { |entity|
-        map_resource(entity, options.dup, &block)
 
-        resource = Resource.new(entity, options)
-        with_options :controller => resource.controller do |map|
-          [:create_confirm].each do |action|
-            route_path = resource.path + '/' + action.to_s
-            route_name = "#{action.to_s}_#{resource.name_prefix}#{resource.singular}"
-
-            map_resource_routes(map, resource, action, route_path, route_name, :post)
-          end
-
-          [:update_confirm].each do |action|
-            route_path = "#{resource.member_path}#{resource.action_separator}" + action.to_s
-            route_name = "#{action.to_s}_#{resource.name_prefix}#{resource.singular}"
-
-            map_resource_routes(map, resource, action, route_path, route_name, :put)
-          end
-        end
+      {
+        :collection => {
+          :create_confirm => :post,
+        },
+        :member => {
+          :update_confirm => :put,
+        }
+      }.each {|type, hash|
+        options[type] ||= {}
+        hash.each{|action, method|
+          options[type][action] ||= method
+        }
       }
+
+      entities << options
+
+      resources(*entities, &block)
     end
   end
 end
