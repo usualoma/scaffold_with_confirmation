c44b97724efba8639fdd3a2d1c495aca4cdc4435
Taku Amano <taku@om.toi-planning.net>
2010-02-09 11:53:03 +0000

    Added support for RSpec.

--- generators/scaffold_with_confirmation/scaffold_with_confirmation_generator.rb
+++ generators/scaffold_with_confirmation/scaffold_with_confirmation_generator.rb
@@ -1,3 +1,5 @@
+require File.dirname(__FILE__) + '/../default_values'
+
 module Rails
   module Generator
     module Commands
@@ -72,7 +74,7 @@ module Rails
 end
 
 class ScaffoldWithConfirmationGenerator < Rails::Generator::NamedBase
-  default_options :skip_timestamps => false, :skip_migration => false, :force_plural => false, :namespace => nil, :skip_confirmation => false
+  default_options :skip_timestamps => false, :skip_migration => false, :force_plural => false, :namespace => nil, :skip_confirmation => false, :rspec => false
 
   attr_reader   :controller_name,
                 :controller_class_path,
@@ -85,7 +87,8 @@ class ScaffoldWithConfirmationGenerator < Rails::Generator::NamedBase
                 :controller_plural_name,
                 :singular_path,
                 :plural_path,
-                :namespace
+                :namespace,
+                :default_file_extension
   alias_method  :controller_file_name,  :controller_underscore_name
   alias_method  :controller_table_name, :controller_plural_name
 
@@ -130,6 +133,8 @@ class ScaffoldWithConfirmationGenerator < Rails::Generator::NamedBase
       @plural_path = path_prefix + '_' + @plural_path
       @singular_path = path_prefix + '_' + @singular_path
     end
+
+    @default_file_extension = "html.erb"
   end
 
   def manifest
@@ -144,9 +149,19 @@ class ScaffoldWithConfirmationGenerator < Rails::Generator::NamedBase
       m.directory(File.join('app/helpers', controller_class_path))
       m.directory(File.join('app/views', controller_class_path, controller_file_name))
       m.directory(File.join('app/views/layouts', controller_class_path))
-      m.directory(File.join('test/functional', controller_class_path))
-      m.directory(File.join('test/unit', class_path))
-      m.directory(File.join('test/unit/helpers', controller_class_path))
+      if ! options[:rspec]
+        m.directory(File.join('test/functional', controller_class_path))
+        m.directory(File.join('test/unit', class_path))
+        m.directory(File.join('test/unit/helpers', controller_class_path))
+      else
+        m.directory(File.join('spec/controllers', controller_class_path))
+        m.directory(File.join('spec/routing', controller_class_path))
+        m.directory(File.join('spec/models', class_path))
+        m.directory(File.join('spec/helpers', class_path))
+        m.directory File.join('spec/fixtures', class_path)
+        m.directory File.join('spec/views', controller_class_path, controller_file_name)
+        m.directory File.join('spec/integration', class_path)
+      end
       m.directory(File.join('public/stylesheets', class_path))
 
       for action in scaffold_views
@@ -170,10 +185,41 @@ class ScaffoldWithConfirmationGenerator < Rails::Generator::NamedBase
       m.template(
         'controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
       )
+      m.template(
+        'helper.rb', File.join('app/helpers', controller_class_path, "#{controller_file_name}_helper.rb")
+      )
 
-      m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
-      m.template('helper.rb',          File.join('app/helpers',     controller_class_path, "#{controller_file_name}_helper.rb"))
-      m.template('helper_test.rb',     File.join('test/unit/helpers',    controller_class_path, "#{controller_file_name}_helper_test.rb"))
+      if ! options[:rspec]
+        m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
+        m.template('helper_test.rb',     File.join('test/unit/helpers',    controller_class_path, "#{controller_file_name}_helper_test.rb"))
+      else
+        # Controller spec, class, and helper.
+        m.template 'routing_spec.rb',
+          File.join('spec/routing', controller_class_path, "#{controller_file_name}_routing_spec.rb")
+
+        m.template 'controller_spec.rb',
+          File.join('spec/controllers', controller_class_path, "#{controller_file_name}_controller_spec.rb")
+
+        m.template 'helper_spec.rb',
+          File.join('spec/helpers', class_path, "#{controller_file_name}_helper_spec.rb")
+
+        # unit test, and fixtures.
+        m.template 'model:fixtures.yml',        File.join('spec/fixtures', class_path, "#{table_name}.yml")
+        m.template 'rspec_model:model_spec.rb', File.join('spec/models', class_path, "#{file_name}_spec.rb")
+
+        # View specs
+        m.template "edit_erb_spec.rb",
+          File.join('spec/views', controller_class_path, controller_file_name, "edit.#{default_file_extension}_spec.rb")
+        m.template "index_erb_spec.rb",
+          File.join('spec/views', controller_class_path, controller_file_name, "index.#{default_file_extension}_spec.rb")
+        m.template "new_erb_spec.rb",
+          File.join('spec/views', controller_class_path, controller_file_name, "new.#{default_file_extension}_spec.rb")
+        m.template "show_erb_spec.rb",
+          File.join('spec/views', controller_class_path, controller_file_name, "show.#{default_file_extension}_spec.rb")
+
+        # Integration
+        m.template 'integration_spec:integration_spec.rb', File.join('spec/integration', class_path, "#{table_name}_spec.rb")
+      end
 
       m.route_resources_with_confirmation controller_file_name
 
@@ -201,6 +247,8 @@ class ScaffoldWithConfirmationGenerator < Rails::Generator::NamedBase
              "Set namespace") { |v| options[:namespace] = v }
       opt.on("--skip-confirmation",
              "Don't generate confirmation page") { |v| options[:skip_confirmation] = v }
+      opt.on("--rspec",
+             "Generate the spec of Rspec instead of the test.") { |v| options[:rspec] = v }
     end
 
     def scaffold_views
@@ -224,4 +272,18 @@ class ScaffoldWithConfirmationGenerator < Rails::Generator::NamedBase
     def model_name
       class_name.demodulize
     end
+end
+
+module Rails
+  module Generator
+    class GeneratedAttribute
+      def input_type
+        @input_type ||= case type
+          when :text                        then "textarea"
+          else
+            "input"
+        end      
+      end
+    end
+  end
 end
